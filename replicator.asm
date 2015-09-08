2aeaea3934b5b6268bb6496f123aca3596b25ad1
Malwaric <brunotavaressilva@gmail.com>
2015-09-08 13:40:38 -0300

    Update and rename replicator.asm to Replicator.asm

--- replicator.asm
+++
@@ -1,7 +0,0 @@
-JMP FAR PTR Duh ; Takes four bytes
-Duh DW V2_Start ; Takes two bytes
-
-DB 1101001B ; Code for JMP(2 Byte-Displacement)
-Duh DW V2_Start - OFFSET Duh ; 2 byte displacement
-
-Duh V2_Start - $
